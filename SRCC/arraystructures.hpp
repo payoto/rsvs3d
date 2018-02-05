89a374b586750eee7de0565d01d292fe31bda485
Alexandre Payot <ap1949@bristol.ac.uk>
2018-02-05 22:43:45 +0000

    Building The classes for handling of grids Added the Hashing to the template ArrayStruct Replaced copying with referencing for the overloaded operator []

--- SRCC/arraystructures.hpp
+++ SRCC/arraystructures.hpp
@@ -3,41 +3,59 @@
 #include <cstdlib>
 #include <string>
 #include <stdexcept>
+#include <unordered_map>
 
 #ifndef ARRAYSTRUCTS_H_INCLUDED
 #define ARRAYSTRUCTS_H_INCLUDED
 
-using namespace std;
+// Defines levels of debugging
+#ifdef DEBUGLVL2 // All Debugging calls
+#define DEBUGLVL1
+#endif
 
+#ifdef DEBUGLVL1 // Debugging of new features.
+#define TEST_ARRAYSTRUCT
+#endif
+
+
+using namespace std;
 template <class T> // Template for array of structures
 class ArrayStruct { 
-   public: 
-      vector<T> elems;    // vector of elements (structures) 
+    public: 
+		vector<T> elems;    // vector of elements (structures) 
+		unordered_map<int,int> hashTable; // Hash Table of indexed elements
+		void HashArray();
+		
 
-      // Hash Table
+		T* operator[](const int& a){ // [] Operator returns a pointer to the corresponding elems.
+			#ifdef TEST_ARRAYSTRUCT // adds a check in debug mode
+				if (a>=elems.size() | 0>elems.size()){
+					throw range_error ("Index is out of range");
+				}
+			#endif
+			return(&(elems[a]));
+		}
 }; 
-
-class ArrayStruct2 {
-public:
-	ArrayStruct2(){ // Constructor
-
-		
+// member function definition
+template<class T> void ArrayStruct <T>::HashArray(){
+	hashTable.reserve(elems.size());
+	for (int i = 0; i < elems.size(); ++i)
+	{
+		hashTable.emplace(elems[i].Key(),i);
 	}
-	~ArrayStruct2(){ // Destructor
+	cout << "Array Struct Succesfully Hashed" << endl;
+}
 
-		
-	}
-
-
-};
 
 class cell {
 public:
 	int index;
 	double fill;
+
 	cell(){ // Constructor
 		index=0;
 		fill=0;
+		cout << "Cell #" << index << " Was created" << endl;
 	}
 	~cell(){ // Destructor
 		cout << "Cell #" << index << " Was deleted" << endl;
@@ -46,20 +64,17 @@ public:
 	disp(){
 		cout << "Cell #" << index << " Fill " << fill << endl;
 	}
+	int Key(){
+		return(index);
+	}
+	operator=( cell* otherCell){
+		index=otherCell->index;
+		fill=otherCell->fill;
+		cout << "HELLO"  << " Other #" << otherCell->index << " Fill " << otherCell->fill << endl;
+		//return(*this);
+	}
 };
 
-typedef struct {
-	int index;
-	int cellind[2];
-	int vertex[2];
-	int orientation; /* 1 is vertical 0 is Horizontal */
-} edge;
-
-typedef struct {
-	int index;
-	double coord[2]; /* needs to be changed wiht dim */
-	
-} vertex;
 
 
 #endif
--- SRCC/test_arraystructures.cpp
+++ SRCC/test_arraystructures.cpp
@@ -9,21 +9,47 @@
 
 
 int main() { 
+	ArrayStruct<int>         intStack;  // stack of ints 
+    ArrayStruct<cell>         cellStack;  // stack of ints 
+   int i;
    try {
-      ArrayStruct<int>         intStack;  // stack of ints 
-      ArrayStruct<cell>         cellStack;  // stack of ints 
+
+      
       cell singleCell;
-      cellStack.elems.push_back(singleCell); 
+
+      cout << "reserve" << endl;
+      //cellStack.elems.reserve(2);
+      cout << "assign" << endl;
+      cellStack.elems.assign(3,singleCell); 
+      cout << "=" << endl;
       singleCell.index=1;
+      cout << "push_back" << endl;
       cellStack.elems.push_back(singleCell); 
 
       // manipulate int stack 
       intStack.elems.push_back(7); 
 
+      cout << "Calls to display" << endl;
       cout << intStack.elems[0] <<endl; 
+      for ( i = 0; i < 4; ++i)
+      {
+      	cellStack.elems[i].index=i;
+      	cellStack[i]->disp() ;
+      }
+      cout << "GAP" << endl;
+      singleCell.index=0;
+      cellStack[0]->disp();
+      cellStack.elems[0]=cellStack[i-1]; 
 
-      cout << cellStack.elems[0].disp() <<endl; 
-      cout << cellStack.elems[1].disp() <<endl; 
+      for ( i = 0; i < 4; ++i)
+      {
+      	cellStack[i]->disp() ;
+      }
+      cout << cellStack.elems.capacity() << endl;
+
+      cellStack.HashArray();
+      
+
 
    } catch (exception const& ex) { 
       cerr << "Exception: " << ex.what() <<endl; 
