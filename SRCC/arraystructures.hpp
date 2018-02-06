66ba8b6853a57873a4efcf0c1daec2aa4108688f
Alexandre Payot <ap1949@bristol.ac.uk>
2018-02-06 18:00:04 +0000

    AUTO-COMMIT For Backup Update

--- SRCC/arraystructures.hpp
+++ SRCC/arraystructures.hpp
@@ -1,14 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <string>
-#include <stdexcept>
-#include <unordered_map>
 
+//===============================================
+// Include Guards
 #ifndef ARRAYSTRUCTS_H_INCLUDED
 #define ARRAYSTRUCTS_H_INCLUDED
 
-// Defines levels of debugging
+//===============================================
+// Levels of debuging Guards
 #ifdef DEBUGLVL2 // All Debugging calls
 #define DEBUGLVL1
 #endif
@@ -17,8 +14,33 @@
 #define TEST_ARRAYSTRUCT
 #endif
 
+//=================================
+// forward declared dependencies
+// 		class foo; //when you only need a pointer not the actual object
+// 		and to avoid circular dependencies
 
+
+//=================================
+// included dependencies
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <stdexcept>
+#include <unordered_map>
+
+//==================================
+// Code
+// NOTE: function in a class definition are IMPLICITELY INLINED 
+//       ie replaced by their code at compile time
 using namespace std;
+typedef unsigned int unsigned_int;
+class cell;
+//template <class T> class ArrayStruct;
+
+typedef ArrayStruct<cell> cellarray;
+
+// Templates
 template <class T> // Template for array of structures
 class ArrayStruct { 
     public: 
@@ -29,24 +51,16 @@ class ArrayStruct {
 
 		T* operator[](const int& a){ // [] Operator returns a pointer to the corresponding elems.
 			#ifdef TEST_ARRAYSTRUCT // adds a check in debug mode
-				if (a>=elems.size() | 0>elems.size()){
+				if ((unsigned_int(a)>=elems.size()) | (0>a)){
 					throw range_error ("Index is out of range");
 				}
 			#endif
 			return(&(elems[a]));
 		}
 }; 
-// member function definition
-template<class T> void ArrayStruct <T>::HashArray(){
-	hashTable.reserve(elems.size());
-	for (int i = 0; i < elems.size(); ++i)
-	{
-		hashTable.emplace(elems[i].Key(),i);
-	}
-	cout << "Array Struct Succesfully Hashed" << endl;
-}
 
 
+// Classes
 class cell {
 public:
 	int index;
@@ -59,22 +73,20 @@ public:
 	}
 	~cell(){ // Destructor
 		cout << "Cell #" << index << " Was deleted" << endl;
-		
 	}
-	disp(){
-		cout << "Cell #" << index << " Fill " << fill << endl;
-	}
-	int Key(){
-		return(index);
-	}
-	operator=( cell* otherCell){
+
+	void disp(){cout << "Cell #" << index << " Fill " << fill << endl;}
+	int Key()    {return(index);}
+
+	void operator=( cell* otherCell){
 		index=otherCell->index;
 		fill=otherCell->fill;
 		cout << "HELLO"  << " Other #" << otherCell->index << " Fill " << otherCell->fill << endl;
-		//return(*this);
 	}
+
 };
 
+// functions
 int test_arraystructures();
 
 #endif
