d40263d94199f4d55ec6a5d9ee76f8b9f6792b3d
Alexandre Payot <ap1949@bristol.ac.uk>
2018-05-31 18:00:05 +0100

    AUTO-COMMIT For Backup Update

---
+++ PrepareCRSVSSource.m
@@ -0,0 +1,11 @@
+function []=PrepareCRSVSSource()
+    % THis function generates the C subroutines necessary for the
+    % calculation and differentiation of Volume and area in 3-Dimensions
+    %
+    % Volume contributions are calculated for a triangle built from an
+    % polygon
+    % Areas are similarly calculated for triangular portions of a polygon
+    
+    
+    
+end
