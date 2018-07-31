b048d177d61fff47e57d3142e30c39a0c6fcb47a
Alexandre Payot <ap1949@bristol.ac.uk>
2018-07-31 20:06:02 +0100

    Add interface to triangulation-RSVSmath
    Unfinished piece of code calculating the full RSVS equations as
    an SQP as required by the RSVS framework.

---
+++ SRCC/RSVSinterface.hpp
@@ -0,0 +1,55 @@
+#ifndef RSVSINTERFACE_H_INCLUDED 
+#define RSVSINTERFACE_H_INCLUDED 
+
+
+//=================================
+// forward declared dependencies
+// 		class foo; //when you only need a pointer not the actual object
+// 		and to avoid circular dependencies
+
+
+//=================================
+// included dependencies
+#include <vector> 
+#include "vectorarray.hpp" 
+#include "RSVSmath.hpp"
+#include "mesh.hpp"
+#include "snake.hpp"
+#include "snakevel.hpp"
+#include <Eigen>
+
+using namespace std; 
+using namespace Eigen; 
+
+class SQPcalc {
+protected:
+	int nDv;
+	int nConstr;
+public :
+	MatrixXd dConstr,HConstr, HObj;
+	RowVectorXd dObj;
+	VectorXd constr, lagMult;
+	double obj=0.0;
+
+
+	HashedVector<int, int> dvMap;
+	HashedVector<int,int> constrMap;
+
+	void BuildMathArrays(int nDv, int nConstr);
+	void BuildConstrMap(const vector<int> &vecin);
+	void BuildDVMap(const vector<int> &vecin);
+	void CalcTriangle(const triangle& triIn, const triangulation &triRSVS);
+	
+};
+
+
+void Deriv1stChainScalar(const MatrixXd &dSdc,const MatrixXd &dcdd, MatrixXd &dSdd);
+void Deriv2ndChainScalar(const MatrixXd &dSdc,const MatrixXd &dcdd,const MatrixXd &HSc,const MatrixXd &Hcd,MatrixXd &HSd);
+void VecBy3DimArray(const MatrixXd &vec, const MatrixXd &arr3dim, MatrixXd &retArray);
+//==================================
+// Code
+// NOTE: function in a class definition are IMPLICITELY INLINED 
+//       ie replaced by their code at compile time
+	
+
+#endif
