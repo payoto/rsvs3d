5b0c3ebfee6c370ef87c3e6a86ca29a9c8141018
Alexandre Payot <ap1949@bristol.ac.uk>
2018-11-30 12:57:55 +0000

    Revamp makefile to support test and all

--- SRCC/src/main.cpp
+++ SRCC/src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
+#include "main.hpp"
 #include "cxxopts.hpp"
 
 int main(int argc, char* argv[])
 {
 
+	CommandLineParser(argc, argv);
+}
+
+
+void CommandLineParser(int argc, char* argv[])
+{
+
+
+	cxxopts::Options options(argv[0], " - example command line options");
+    options
+      .positional_help("[optional args]")
+      .show_positional_help();
+	
 }
