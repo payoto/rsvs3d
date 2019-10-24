b9c3e2721de5b814a6b885206626daca86783306
alexandre payot <alexandre_payot@hotmail.com>
2019-10-24 13:59:37 +0100

    Add description for "systemnomatlab"

--- SRCMAT/systemnomatlab.m
+++ SRCMAT/systemnomatlab.m
@@ -1,5 +1,16 @@
 function [status,result]=systemnomatlab(command)
-    
+    % Trims Matlab libraries from the PATH variable on a system call
+    %
+    % MATLAB has the habit of prepending itself to the path ruining all kinds of
+    % dependencies during system calls with very hard to trace back error 
+    % messages.
+    % This function is called as like the matlab "system" but the PATH variable 
+    % is edited to not have any path with the matlab prepended.
+    % 
+    % May cause a failure if you Need some of those matlab libraries later in
+    % the path, especially if Matlab is actually first on your path, then 
+    % probably you shouldn't need this anyway.
+
     [~, matlabedPath]=system('echo %PATH%');
     
     if isunix
