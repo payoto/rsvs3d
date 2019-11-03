payoto/rsvs3d

commit ad4bd6cfd07e70a0992d4b98857d0d70096e4a23

Fix TriarrayToMesh
The triangulation to mesh algorithm was broken by the calculation of
edgelengths in prepareforuse

SRCC/incl/arraystructures.hpp
@@ -162,7 +162,7 @@ public:
 			std::cerr << stacktrace::stacktrace() << std::endl;
 			#endif
 			std::cerr << "Attempt to access position " << a << 
-				" in std::array of size " << elems.size() << std::endl;
+				" in std::vector of size " << elems.size() << std::endl;
 			// dbg::fail(__PRETTY_FUNCTION__,"index out of range");
 			RSVS3D_ERROR_RANGE(" Index is out of range");
 		}
@@ -179,7 +179,7 @@ public:
 			std::cerr << stacktrace::stacktrace() << std::endl;
 			#endif
 			std::cerr << "Attempt to access position " << a << 
-				" in std::array of size " << elems.size() << std::endl;
+				" in std::vector of size " << elems.size() << std::endl;
 			// dbg::fail(__PRETTY_FUNCTION__,"index out of range");
 			RSVS3D_ERROR_RANGE(" Index is out of range");
 		}
@@ -198,7 +198,7 @@ public:
 			#endif
 			std::cerr << "Attempt to access index " << b 
 				<< " at position " << a <<
-				" in std::array of size " << elems.size() << std::endl;
+				" in std::vector of size " << elems.size() << std::endl;
 			// dbg::fail(__PRETTY_FUNCTION__,"index out of range");
 			RSVS3D_ERROR_RANGE(" Index is out of range");
 		}
@@ -215,7 +215,7 @@ public:
 			std::cerr << stacktrace::stacktrace() << std::endl;
 			#endif
 			std::cerr << "Attempt to access position " << a << 
-				" in std::array of size " << elems.size() << std::endl;
+				" in std::vector of size " << elems.size() << std::endl;
 			// dbg::fail(__PRETTY_FUNCTION__,"index out of range");
 			RSVS3D_ERROR_RANGE("Index is out of range");
 		}

SRCC/incl/arraystructures_incl.cpp
@@ -598,6 +598,7 @@ template<class T> void ArrayStruct <T>::PopulateIndices()
 	isSetMI=1;
 	isHash=0;
 	readyforuse=false;
+	this->HashArray();
 }
 template<class T> void ArrayStruct <T>::ChangeIndices(int nVert, int nEdge,
 	int nSurf, int nVolu)

SRCC/incl/triangulate.hpp
@@ -246,7 +246,9 @@ SurfCentroid SurfaceCentroid_SnakeSurf(const surf &surfin,
 int Test_snakeRSVS();
 int Test_surfcentre();
 int Test_snakeRSVS_singlevol();
-int Test_RSVSalgo_singlevol();
 int Test_MeshOrient();
+int Test_RSVSalgo_singlevol(int sparseCuttoff);
+int Test_RSVSalgo_singlevol_fullmath();
+int Test_RSVSalgo_singlevol_sparse();
 #endif // TRIANGULATE_H_INCLUDED
 

SRCC/makefile
@@ -92,8 +92,7 @@ endif
 # -funroll-all-loops -ffast-math -ffinite-math-only 
 # Tetgen flag to use as library: -DTETLIBRARY
 # cxxopts #
-CCFLAGDBG= 
-#-g -ggdb
+CCFLAGDBG= -g -ggdb
 # -DDEBUGLVL1 -DSAFE_ACCESS -DSAFE_ALGO -DTIME_EXEC -DRSVS_DIAGNOSTIC
 CCFLAGPROJ= -DRSVS_DEBUG -DSAFE_ACCESS -DSAFE_ALGO -DTIME_EXEC -DRSVS_ACCESS_DEVELOPMENT_PARAMETERS
 # 

SRCC/src/snake/triangulate.cpp
@@ -483,12 +483,7 @@ mesh TriarrayToMesh(const triangulation& triangul, const triarray& triin){
 	meshout.Init(nVe, nE, nSurf, nVolu);
 	meshout.PopulateIndices();
 	meshout.volus.HashArray();
-	meshout.PrepareForUse();
-	// cout << endl ; 
-	// for(ii=0; ii<nVolu; ++ii){
-	// 	cout << meshout.volus(ii)->index << " " ; 
-	// }
-	// cout << endl ; 
+	meshout.HashArray();
 	
 	for(ii=0; ii<nSurf; ++ii){
 		// cout << endl;
@@ -505,7 +500,7 @@ mesh TriarrayToMesh(const triangulation& triangul, const triarray& triin){
 		if(meshout.surfs[ii].voluind.size()==1){
 			meshout.surfs[ii].voluind.push_back(0);
 		}
-		if(triin(ii)->connec.constrinfluence[0]==1.0){
+		if(IsAproxEqual(triin(ii)->connec.constrinfluence[0],1.0)){
 			kk=meshout.surfs[ii].voluind[0];
 			meshout.surfs[ii].voluind[0] = meshout.surfs[ii].voluind[1];
 			meshout.surfs[ii].voluind[1]=kk;
@@ -515,14 +510,16 @@ mesh TriarrayToMesh(const triangulation& triangul, const triarray& triin){
 			kk=meshout.volus.find(meshout.surfs[ii].voluind[jj], true);
 			if(kk>=0){
 				meshout.volus[kk].surfind.push_back(ii+1);
-			} else { cerr << " w " << kk << " " << meshout.surfs[ii].voluind[jj]
-				<< " " << triin(ii)->connec.celltarg[jj];
+			} 
+			else if (meshout.surfs[ii].voluind[jj]!=0) {
+				std::cerr << " w " << kk << " " << meshout.surfs[ii].voluind[jj]
+					<< " " << triin(ii)->connec.celltarg[jj];
 				// meshout.volus[meshout.surfs[ii].voluind[jj]-1].surfind.push_back(ii+1);
 			}
 		}
 		
 		for(jj =0; jj<3; jj++){
-			meshout.edges[ii*3+jj].surfind = {ii};
+			meshout.edges[ii*3+jj].surfind = {meshout.surfs(ii)->index};
 			meshout.edges[ii*3+jj].vertind = {ii*3+1+jj, ii*3+1+((jj+1)%3)};
 			if (triin(ii)->pointtype[jj]==meshtypes::mesh){
 				meshout.verts[ii*3+jj].coord=triangul.meshDep->

SRCC/src/test/snakstruct_test.cpp
@@ -1317,7 +1317,8 @@ void repositionatquarteredge(snake &snakein){
 	snakein.UpdateCoord();
 }
 
-int Test_RSVSalgo_singlevol(){
+
+int Test_RSVSalgo_singlevol(int sparseCutOff){
 	// int nVoluZone, ii;
 
 	snake testSnake;
@@ -1383,8 +1384,8 @@ int Test_RSVSalgo_singlevol(){
 		triRSVS.PrepareForUse();
 		triRSVS.CalcTriVertPos();
 		MaintainTriangulateSnake(triRSVS);
-
-		for(ii=0;ii<20;++ii){
+		calcObj.SetSparseDVcutoff(sparseCutOff);
+		for(ii=0;ii<2;++ii){
 			cout << ii << " ";
 			// if (ii%2==0){
 				PrintRSVSSnake(outSnake, testSnake, totT, testTriangle,
@@ -1403,37 +1404,62 @@ int Test_RSVSalgo_singlevol(){
 		stop_s=clock();
 		cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << "ms" << endl;
 		testSnake.displight();
+		testSnake.PrepareForUse();
 		// outSnake.PrintMesh(testSnake.snakeconn);
 		// outSnake.PrintSnakeInternalPts(testSnake);
 		
 		repositionatquarteredge(testSnake);
 		triRSVS.CalcTriVertPos();
 		triRSVS.PrepareForUse();
-		triMesh2= TriarrayToMesh(triRSVS, triRSVS.intertri);
 		FILE *fid;
-		fid = fopen("../TESTOUT/triintertestbig.dat","w");
-		triMesh2.write(fid);//(triMesh,3,totT);
-		fclose(fid);
-		/*fid = fopen("../TESTOUT/snakeconnouttri.dat","w");
-		triMesh3= TriarrayToMesh(triRSVS, triRSVS.dynatri);
-		// testSnake.snakeconn.write(fid);//(triMesh,3,totT);
-		triMesh3.write(fid);//(triMesh,3,totT);
-		fclose(fid);
+		try {
+			triMesh2= TriarrayToMesh(triRSVS, triRSVS.intertri);
+			fid = fopen("../TESTOUT/triintertestbig.dat","w");
+			triMesh2.PrepareForUse();
+			triMesh2.write(fid);//(triMesh,3,totT);
+			fclose(fid);
+		} catch (exception const& extriMesh2) { 
+			RSVS3D_ERROR_NOTHROW("intertri output fails");
+			errTest++;
+		} 
 
-		fid = fopen("../TESTOUT/snakeconnout.dat","w");		
-		testSnake.snakeconn.write(fid);//(triMesh,3,totT);
-		fclose(fid);
+		try {
+			triMesh3= TriarrayToMesh(triRSVS, triRSVS.dynatri);
+			fid = fopen("../TESTOUT/snakeconnouttri.dat","w");
+			triMesh3.PrepareForUse();
+			triMesh3.write(fid);//(triMesh,3,totT);
+			fclose(fid);
+		} catch (exception const& extriMesh2) { 
+			RSVS3D_ERROR_NOTHROW("dynatri output fails");
+			errTest++;
+		} 
 
-		fid = fopen("../TESTOUT/stattri.dat","w");
-		triMesh4= TriarrayToMesh(triRSVS, triRSVS.stattri);
-		triMesh4.write(fid);//(triMesh,3,totT);
-		fclose(fid);*/
+		try {
+			triMesh4= TriarrayToMesh(triRSVS, triRSVS.stattri);
+			fid = fopen("../TESTOUT/stattri.dat","w");
+			triMesh4.PrepareForUse();
+			triMesh4.write(fid);//(triMesh,3,totT);
+			fclose(fid);
+		} catch (exception const& extriMesh2) { 
+			RSVS3D_ERROR_NOTHROW("stattri output fails");
+			errTest++;
+		} 
 
 	} catch (exception const& ex) { 
 		cerr << "Exception: " << ex.what() <<endl; 
 		return -1;
 	} 
 	return(errTest);
+}
+
+
+int Test_RSVSalgo_singlevol_fullmath(){
+	return(Test_RSVSalgo_singlevol(10000000));
+}
+
+
+int Test_RSVSalgo_singlevol_sparse(){
+	return(Test_RSVSalgo_singlevol(0));
 }
 
 int Test_snakeRSVS_singlevol(){
@@ -1586,7 +1612,7 @@ void Test_stepalgoRSVS(snake &testSnake,triangulation &RSVStri , vector<double> 
 	// calcObj.Print2Screen(2);
 	CalculateNoNanSnakeVel(testSnake);
 	outSnake2.PrintSnake(testSnake, 1, totT);  
-	testSnake.CalculateTimeStep(dt,0.9);
+	testSnake.CalculateTimeStep(dt,0.4);
 	testSnake.UpdateDistance(dt,0.9,true);
 	testSnake.PrepareForUse();
 	testSnake.UpdateCoord();

SRCC/src/test/test.cpp
@@ -64,7 +64,8 @@ int rsvstest::maintest(){
 	gridTest.RunSilent(Test_RSVSalgo,"Snake RSVS from spawn");
 	gridTest.RunSilent(Test_snakeRSVS,"Snake RSVS");
 	gridTest.RunSilent(Test_snakeRSVS_singlevol,"Snake RSVS single vol");
-	gridTest.RunSilent(Test_RSVSalgo_singlevol,"Snake RSVS algorithm single vol");
+	gridTest.RunSilent(Test_RSVSalgo_singlevol_fullmath,"Snake RSVS algorithm (full maths)");
+	gridTest.RunSilent(Test_RSVSalgo_singlevol_sparse,"Snake RSVS algorithm (sparse)");
 	// Parameter and JSON tests
 	gridTest.RunSilent(param::test::base,"parameter implementation");
 	gridTest.RunSilent(param::test::symmetry,"Test json internal symmetry");
@@ -96,24 +97,23 @@ int rsvstest::maintest(){
 int rsvstest::newtest(){
 	customtest gridTest("3-D RSVS tests: New and breaking ");
 
-	// #ifdef TEST_KNOWN_FAILURES
-	// gridTest.RunSilent(Test_snakeinitflat,"Snake spawning 2D");
-	// gridTest.Run(Test_RSVSalgoflat,"RSVS 2D"); // Non working test - Maths not finished	
-	// gridTest.Run(integrate::test::CompareDerivativesSpike,"Test spike issues"); 
-	// gridTest.Run(integrate::test::CompareDerivativesSpikeNoDPos,"Test spike issues no dPos"); 
-	// gridTest.Run(integrate::test::StudyDerivatives,"Generate Derivative study"); 
-	// #endif
-	// // Parameter and JSON tests
-	// gridTest.RunSilent(param::test::base,"parameter implementation");
-	// gridTest.RunSilent(param::test::symmetry,"Test json internal symmetry");
-	// gridTest.RunSilent(param::test::io,"parameter read write");
-	// gridTest.RunSilent(param::test::ioflat,"parameter read write flat format");
-	// gridTest.RunSilent(param::test::ipartialread,"parameter partial read"
-	// 	" write flat format");
-	// gridTest.RunSilent(param::test::autoflat,"Algorithm for automatic "
-	// 	"determination of flat json");
+	#ifdef TEST_KNOWN_FAILURES
+	gridTest.RunSilent(Test_snakeinitflat,"Snake spawning 2D");
+	gridTest.Run(Test_RSVSalgoflat,"RSVS 2D"); // Non working test - Maths not finished	
+	gridTest.Run(integrate::test::CompareDerivativesSpike,"Test spike issues"); 
+	gridTest.Run(integrate::test::CompareDerivativesSpikeNoDPos,"Test spike issues no dPos"); 
+	gridTest.Run(integrate::test::StudyDerivatives,"Generate Derivative study"); 
+	#endif
+	// Parameter and JSON tests
+	gridTest.RunSilent(param::test::base,"parameter implementation");
+	gridTest.RunSilent(param::test::symmetry,"Test json internal symmetry");
+	gridTest.RunSilent(param::test::io,"parameter read write");
+	gridTest.RunSilent(param::test::ioflat,"parameter read write flat format");
+	gridTest.RunSilent(param::test::ipartialread,"parameter partial read"
+		" write flat format");
+	gridTest.RunSilent(param::test::autoflat,"Algorithm for automatic "
+		"determination of flat json");
 	
-	gridTest.Run(Test_RSVSalgo_singlevol,"Snake RSVS algorithm single vol");
 
 	return(0);
 }

commit 6c033ff945a29c78e6f55a61dd99bfd65792ac02

Fix Readiness test after PopulateIndices
Fix test failure introduced in ad4bd6cfd07e70a0992d4b98857d0d70096e4a23
The test now failed because checkready and isready did not always match
after populating indices, which is actually fine.

Indeed while the arraystruct is Hashed and Maxindex is set, the elements
may not be ready, (and it may not be possible to test) as only the index
operations have been performed.

SRCC/incl/arraystructures.hpp
@@ -468,7 +468,8 @@ public:
 
 // functions
 template <class T> bool CompareDisp(T *mesh1,T *mesh2);
-template<class T> int TestReadyness(T &stackT, const char* txt, bool errTarg);
+template<class T> int TestReadiness(T &stackT, const char* txt, bool errTarg, 
+	bool errTargCheck);
 template<class T> void DisplayVector(std::vector<T> vec);
 template<class T> void DisplayVectorStatistics(std::vector<T> vec);
 template<class T> void PrintVector(std::vector<T> vec, std::ostream &streamout);

SRCC/incl/arraystructures_incl.cpp
@@ -240,18 +240,18 @@ template <class T> int TestTemplate_ArrayStruct()
 				"working (Stage 2)" << std::endl;
 			errFlag++;
 		} 
-		errFlag+=TestReadyness(stackT," after assignement",false);
+		errFlag+=TestReadiness(stackT," after assignement",false,false);
 
 		// Test prepare
 		stackT.PrepareForUse();
 		stackT2.PrepareForUse();
-		errFlag+=TestReadyness(stackT," after PrepareForUse",true);
+		errFlag+=TestReadiness(stackT," after PrepareForUse",true,true);
 
 		// Test Find
 		i=stackT.find(10);
 		j=stackT.find(7);
 		testSub=stackT.find_list(testSub);
-		errFlag+=TestReadyness(stackT," after find",true);
+		errFlag+=TestReadiness(stackT," after find",true,true);
 
 
 		if (i!=2 || j!=rsvs3d::constants::__notfound){
@@ -267,7 +267,7 @@ template <class T> int TestTemplate_ArrayStruct()
 
 		try{
 			stackT[2].index=7;
-			errFlag+=TestReadyness(stackT," after [] assignement",false);
+			errFlag+=TestReadiness(stackT," after [] assignement",false,false);
 
 			i=stackT.find(10);
 			// If this does not throw an error with have an issue
@@ -286,25 +286,25 @@ template <class T> int TestTemplate_ArrayStruct()
 		// Test Concatenation of arrays
 
 		stackT.PrepareForUse();
-		errFlag+=TestReadyness(stackT," after PrepareForUse (2nd Time)",true);
+		errFlag+=TestReadiness(stackT," after PrepareForUse (2nd Time Time)",true,true);
 
 		stackT.Concatenate(stackT2);
-		errFlag+=TestReadyness(stackT," after Concatenate",false);
+		errFlag+=TestReadiness(stackT," after Concatenate",false,false);
 
 		stackT.PrepareForUse();
-		errFlag+=TestReadyness(stackT," after PrepareForUse (3nd Time)",true);
+		errFlag+=TestReadiness(stackT," after PrepareForUse (3nd Time Time)",true,true);
 
 		stackT.PopulateIndices();
-		errFlag+=TestReadyness(stackT," after PopulateIndices",false);
+		errFlag+=TestReadiness(stackT," after PopulateIndices",false,true);
 
 		stackT.PrepareForUse();
-		errFlag+=TestReadyness(stackT," after PrepareForUse (4th Time)",true);
+		errFlag+=TestReadiness(stackT," after PrepareForUse (4th Time Time)",true,true);
 
 		// Test Read write:
 		fidw=fopen("../TESTOUT/testarray.dat","w");
 		if(fidw!=NULL){
 			stackT.write(fidw);
-			errFlag+=TestReadyness(stackT," Write out",true);
+			errFlag+=TestReadiness(stackT," Write out",true,true);
 			fclose(fidw);
 			fidr=fopen("../TESTOUT/testarray.dat","r");
 			if(fidr!=NULL){
@@ -337,7 +337,7 @@ template <class T> int TestTemplate_ArrayStruct()
 			errFlag++;
 		}
 		stackT.disp();
-		errFlag+=TestReadyness(stackT," Disp",true);
+		errFlag+=TestReadiness(stackT," Disp",true,true);
 		
 		stackT.elems.erase(stackT.elems.begin(),++(++stackT.elems.begin()));
 		stackT.isHash=0;
@@ -359,7 +359,8 @@ template <class T> int TestTemplate_ArrayStruct()
 	return(errFlag);
 }
 
-template<class T> int TestReadyness(T &stackT, const char* txt, bool errTarg)
+template<class T> int TestReadiness(T &stackT, const char* txt, bool errTarg,
+	bool errTargCheck)
 {
 	// Test if the behaviour of the readyness test is as expected
 	// stackT is the class to be tested 
@@ -374,7 +375,7 @@ template<class T> int TestReadyness(T &stackT, const char* txt, bool errTarg)
 		errFlag++;
 	} 
 	errTest=stackT.checkready();
-	if (!(errTest==errTarg)){
+	if (!(errTest==errTargCheck)){
 		std::cerr << "stackT wrongly marked as " << (errTarg ? "not" : "" ) 
 			<< " ready (checkready())"  << txt  << std::endl;
 		errFlag++;