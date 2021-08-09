8ead09b30f7f9afdedbc0816aac730715725bafd
Alexandre Payot <alexandre.payotli@gmail.com>
2021-08-09 13:45:45 +0200

    Add runtime checks to the mesh integration

--- SRCC/src/interfaces/polyscopersvs.cpp
+++ SRCC/src/interfaces/polyscopersvs.cpp
@@ -47,10 +47,15 @@ void polyscopersvs::PolyScopeRSVS::show(size_t forFrames)
  *
  * @param name The name of the surface mesh in polyscope. Using an existing name will allow to
  *  overwrite a previous mesh.
- * @param meshIn The mesh object that will be parsed for polyscope.
+ * @param meshIn The mesh object that will be parsed for polyscope. This mesh must have been
+ * prepared (Using the `mesh::PrepareForUse` method).
  */
 void polyscopersvs::PolyScopeRSVS::addMesh(std::string name, const mesh &meshIn)
 {
+    if (!meshIn.isready())
+    {
+        RSVS3D_ERROR("Mesh is not ready, cannot add to polyscope. Call `PrepareForUse` first.");
+    }
     std::vector<std::vector<double>> points;
     std::vector<std::vector<int>> faces;
     points.reserve(meshIn.verts.size());
@@ -58,24 +63,46 @@ void polyscopersvs::PolyScopeRSVS::addMesh(std::string name, const mesh &meshIn)
 
     for (size_t i = 0; i < meshIn.verts.size(); ++i)
     {
+        if (meshIn.verts(i)->coord.size() != 3)
+        {
+            RSVS3D_ERROR_RANGE(
+                (std::string("Vertex at position ") + std::to_string(i) + " has invalid coordinate.").c_str());
+        }
+
         points.push_back(meshIn.verts(i)->coord);
     }
     for (size_t i = 0; i < meshIn.surfs.size(); ++i)
     {
-        auto tmp = meshIn.verts.find_list(meshIn.surfs(i)->OrderedVerts(&meshIn));
-        std::cout << tmp.size() << std::endl;
-        faces.push_back(tmp);
+        auto faceIndices = meshIn.verts.find_list(meshIn.surfs(i)->OrderedVerts(&meshIn));
+        if (faceIndices.size() < 3)
+        {
+            RSVS3D_ERROR_RANGE(
+                (std::string("Surface at position") + std::to_string(i) + " has less than 3 vertices.").c_str());
+        }
+        faces.push_back(faceIndices);
     }
 
     polyscope::registerSurfaceMesh(name, points, faces);
 }
 
+/**
+ * @brief Should initialise polyscope
+ *
+ * @return int The error code: 0 indicates succes, all other numbers are failures
+ */
+
 int polyscopersvs::test::init()
 {
     polyscopersvs::PolyScopeRSVS viewer;
+    assert(polyscopersvs::POLYSCOPE_INITIALISED);
     return 0;
 }
 
+/**
+ * @brief Should display the polyscope window
+ *
+ * @return int The error code: 0 indicates succes, all other numbers are failures
+ */
 int polyscopersvs::test::show()
 {
     polyscopersvs::PolyScopeRSVS viewer;
@@ -83,6 +110,11 @@ int polyscopersvs::test::show()
     return 0;
 }
 
+/**
+ * @brief Should display a single small cubic mesh
+ *
+ * @return int The error code: 0 indicates succes, all other numbers are failures
+ */
 int polyscopersvs::test::meshShow()
 {
     int errorOut = 0;
@@ -91,7 +123,6 @@ int polyscopersvs::test::meshShow()
 
     std::array<int, 3> dims = {2, 3, 4};
     errorOut += BuildBlockGrid(dims, mesh1);
-    std::cout << "mesh1.verts.size() = " << mesh1.verts.size() << std::endl;
     mesh1.PrepareForUse();
     viewer.addMesh("mesh1", mesh1);
     viewer.show(60);
